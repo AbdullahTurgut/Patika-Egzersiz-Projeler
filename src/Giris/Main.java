71536795b884a82830af954eedb9fb6665d43441
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-03-31 01:29:24 +0300

    Tutar hesaplama ödevi kodlandı

--- src/Giris/Main.java
+++ src/Giris/Main.java
@@ -4,6 +4,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Egzersiz : Java ile kullanıcıdan alınan para değerinin KDV'li fiyatını
+        // ve KDV tutarını hesaplayıp ekrana bastıran programı yazın.
+        Scanner scanner = new Scanner(System.in);
+
+        double tutar,kdvTutar,kdvliTutar;
+        double kdvOran = 0.18;
+
+        System.out.print("Lütfen TL para değeri giriniz: ");
+        tutar = scanner.nextDouble();
+
+        kdvOran = (tutar < 1000) ? 0.18 : 0.8;
+        kdvTutar = tutar * kdvOran;
+        kdvliTutar = tutar + kdvTutar;
+
+        System.out.println("KDV'siz tutar: " + tutar);
+        System.out.println("KDV oranı: " + kdvOran);
+        System.out.println("KDV tutarı: " + kdvTutar);
+        System.out.println("KDV'li tutarı: " + kdvliTutar);
+
+    }
+
+    private static void NotOrtalamasi() {
         // Egzersiz : Not ortalaması bulan program
 
         int mat,fiz,kim,turk,tarih,muzik;
@@ -26,8 +49,5 @@ public class Main {
         String sonuc = (ort > 60) ? "Geçti" : "Kaldı";
         System.out.println("Ortalama " + ort + " \'dır");
         System.out.println("Sonuc : " + sonuc );
-
-
-
     }
 }
