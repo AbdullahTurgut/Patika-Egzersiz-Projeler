10cf75ff9303935936b3fa99d2f70009de8fe3ad
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-04-19 05:34:37 +0300

    PrintAwithStars sınıfı kodlandı.

---
+++ src/ArraysExamples/PrintAwithStars.java
@@ -0,0 +1,36 @@
+package ArraysExamples;
+
+public class PrintAwithStars {
+    public static void main(String[] args) {
+        // Çok boyutlu diziler kullanılarak
+        // "yıldızlar" ile ekrana "B" harfi yazan program
+        String[][] letterB = new String[7][4];
+
+        // * ile B harfi için nested for
+        for (int i = 0; i < letterB.length; i++) {
+            for (int j = 0; j < letterB[i].length; j++) {
+                if (i == 0 && j == 3)
+                    letterB[i][j] = "   ";
+                else if( i == 3 && j == 3)
+                    letterB[i][j] = "   ";
+                else if( i == 6 && j == 3)
+                    letterB[i][j] = "   ";
+                else if (i == 0 || i == 3 || i == 6)
+                    letterB[i][j] = " * ";
+                else if (j == 0 || j == 3) {
+                    letterB[i][j] = " * ";
+                } else
+                    letterB[i][j] = "   ";
+            }
+        }
+
+        // diziyi bastırmak için foreach
+        for (String[] row : letterB) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+
+    }
+}
