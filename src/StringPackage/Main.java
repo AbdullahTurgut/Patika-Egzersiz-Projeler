630ad139f3e79836cf17b082a02111bfb88b11fb
AbdullahTurgut <abdullah.trgt4196@gmail.com>
2023-04-21 11:04:00 +0300

    StringPackage klasörü oluşturuldu.

---
+++ src/StringPackage/Main.java
@@ -0,0 +1,10 @@
+package StringPackage;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "patika";
+        System.out.println(str.charAt(2)); // t harfini bastırır
+        System.out.println(str.codePointAt(1)); // stringin belirtilen indexinin ASCII karşılığını verir
+
+    }
+}
